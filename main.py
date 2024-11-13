e096376099d8d48fafa7e77068775f9dc90c84a9
Phuong-Thanh Ho <hophuongthanh028@gmail.com>
2024-11-13 09:46:31 -0500

    demo works

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from navigate import Navigate
 
 
 navigate = Navigate((2, 2), 0, Manhattan())
-for destination in [(3, 2), (3, 1), (5,1), (5,0)]:
+for destination in [(4,3), (6,2), (1,1)]:
     print('----------------')
     path = navigate.compute_path(destination)
     navigate.drive_path()
